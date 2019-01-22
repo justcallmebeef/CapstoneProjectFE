325cc476641c2e124542d22ec7563a5f14d4438a
justcallmebeef <bethanymitch10@gmail.com>
2019-01-22 14:36:56 -0700

    added logo images in both black and white and updated that image in the App to make sure it pulled in correctly

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import asendWhite from './asend-white.png';
 import './App.css';
+
+const climberAPI = 'https://capstone-climb-db.herokuapp.com/climbers'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={asendWhite} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
