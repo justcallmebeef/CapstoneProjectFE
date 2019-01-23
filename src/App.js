eb899b26d27014db251927b477f5140a4b75f8b9
justcallmebeef <bethanymitch10@gmail.com>
2019-01-23 13:33:18 -0700

    moved components folder and added the Welcome to the App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import asendWhite from './asend-white.png';
+import React, { Component, Fragment } from 'react'
+import asendWhite from './asend-white.png'
 import { Route, Link } from 'react-router-dom'
+import Welcome from './components/Welcome'
 import './App.css';
 
 const climberAPI = 'https://capstone-climb-db.herokuapp.com/climbers'
@@ -9,7 +10,11 @@ const routesAPI = 'https://capstone-climb-db.herokuapp.com/routes'
 class App extends Component {
   render() {
     return (
+      <Fragment>
+      
+      <Welcome />
       <img src={asendWhite} alt="logo" />
+      </Fragment>
     );
   }
 }
