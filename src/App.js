1c7d4fe3093a0bb0dc20ed69b4651712415ac75c
justcallmebeef <bethanymitch10@gmail.com>
2019-01-23 13:13:46 -0700

    installed react router dom and built a Welcome component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import asendWhite from './asend-white.png';
+import { Route, Link } from 'react-router-dom'
 import './App.css';
 
 const climberAPI = 'https://capstone-climb-db.herokuapp.com/climbers'
+const routesAPI = 'https://capstone-climb-db.herokuapp.com/routes'
 
 class App extends Component {
   render() {
