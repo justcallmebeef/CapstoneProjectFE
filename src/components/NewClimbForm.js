730d399ab9049ff0e136c93ae605da0f4263363f
justcallmebeef <bethanymitch10@gmail.com>
2019-01-30 11:46:57 -0700

    added select option to the climb form so the grades automatically populate, and cleaned it up a bit with css

--- src/components/NewClimbForm.js
+++ src/components/NewClimbForm.js
@@ -1,25 +1,55 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Link } from 'react-router-dom'
 
 const NewClimbForm = () => {
     return (
+        <Fragment>
         <div>
             <form>
                 <div class="input-container">
-                    <i class="fa fa-star icon"></i>
-                    <input class="input-field" type="text" placeholder="Route Name" name="route_name"></input>
+                    <i className="fa fa-star icon"></i>
+                    <input className="input-field" type="text" placeholder="Route Name" name="route_name"></input>
                 </div>
                 
-                <div class="input-container">
-                    <i class="fa fa-paper-plane-o icon"></i>
-                    <select class="input-field" type="text" placeholder="Route Grade" name="route_grate">
-                        <option>Test</option>
-                        <option>Test2</option>
+                <div className="input-container">
+                    <i className="fa fa-paper-plane-o icon"></i>
+                    <select className="input-field" placeholder="Route Grade" name="route_grade">
+                        <option selected disabled>Route Grade</option>
+                        <option>Less than 5.6</option>
+                        <option>5.6</option>
+                        <option>5.7</option>
+                        <option>5.8</option>
+                        <option>5.9</option>
+                        <option>5.10a</option>
+                        <option>5.10b</option>
+                        <option>5.10c</option>
+                        <option>5.10d</option>
+                        <option>5.11a</option>
+                        <option>5.11b</option>
+                        <option>5.11c</option>
+                        <option>5.11d</option>
+                        <option>5.12a</option>
+                        <option>5.12b</option>
+                        <option>5.12c</option>
+                        <option>5.12d</option>
+                        <option>5.13a</option>
+                        <option>5.13b</option>
+                        <option>5.13c</option>
+                        <option>5.13d</option>
+                        <option>5.14a</option>
+                        <option>5.14b</option>
+                        <option>5.14c</option>
+                        <option>5.14d</option>
+                        <option>5.15a</option>
+                        <option>5.15b</option>
+                        <option>5.15c</option>
+                        <option>5.15d</option>
                     </select>
                 </div>
                 <Link to='/climbingStats'><button type="button" class="btn">ASEND it!</button></Link>
             </form>
         </div>
+        </Fragment>
     )
 }
 
